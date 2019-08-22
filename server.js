052703c2a2f2ec9f4461b0a172dea0276e078953
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-08-21 21:48:37 -0500

    implemented routes

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) => res.json({ msg: 'Welcome to ContactsKeeper API...'}))
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to ContactsKeeper API...' })
+);
 
 //Define Routes
 
-app.use('/api/users' , require('./routes/users'))
-app.use('/api/auth' , require('./routes/auth'))
-app.use('/api/contacts' , require('./routes/contacts'))
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , () =>console.log(`server started on ${PORT}`))
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
