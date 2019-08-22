d5a6fbda541a4c18b5af05adf7ccfe29f2d6bc23
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-08-21 21:47:59 -0500

    implemented routes

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 // @route  POST   api/users
 //@desc           register a user
 // @access        Public
 router.post('/', (req, res) => {
-    res.send('Register a user')
+  res.send('Register a user');
 });
 
-module.exports = router
+module.exports = router;
