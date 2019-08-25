ba898108ebf5c0f13e8a3a94e0f4326e4e52a99a
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-08-25 17:29:02 -0500

    Database connected to MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 const app = express();
 
 //COnnect to Database
-connectDB()
+connectDB();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to ContactsKeeper API...' })
@@ -18,4 +18,3 @@ app.use('/api/contacts', require('./routes/contacts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
-
