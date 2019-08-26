161e413d38308ffd53db70e24a229e9769ac583e
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-08-25 20:59:21 -0500

    Hashing passwords and creating webTokens

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator');
-
+const jwt = require('jsonwebtoken')
 const User = require('../models/User');
 
 // @route  POST   api/users
@@ -19,14 +20,45 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
-  }
+    const {name , email , password} = req.body
+
+    try {
+      let user = await User.findOne({ email })
+
+      if(user) {
+        return res.status(400).json({ msg: 'User already exists' })
+      }
+
+      user = new User ({
+        naem,
+        email,
+        password
+      })
+
+      const salt = await bcrypt.genSalt(10)
+
+      user.password = await bcrypt.hash(password , salt)
+
+      await user.save()
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error')
+    }
+
+    }
 );
 
 module.exports = router;
