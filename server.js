f6aaa63a99546342d7f61593b8c5088e782613c2
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-08-25 20:24:18 -0500

    implement user models

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 
-
 //COnnect to Database
 connectDB();
 
 //init Middleware
-app.use(express.json({ extended:false }))
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to ContactsKeeper API...' })
