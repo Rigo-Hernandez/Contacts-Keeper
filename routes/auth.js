8704e1adb157961ecf1963603ca7ff4cee1a4395
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-09-02 22:57:28 -0500

    Authenticated routes token is given when credentials match

--- routes/auth.js
+++ routes/auth.js
@@ -13,9 +13,9 @@ router.get('/', (req, res) => {
   res.send('Get logged in user');
 });
 
-// @route  POST    api/auth
-//@desc           Auth user and get token
-// @access        Public
+// @route  POST    api/auth   
+//@desc           Auth user and get token  
+// @access        Public  
 router.post(
   '/',
   [
