318b22757e45fb4e3ab936cc804c7c3578cd9632
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-09-09 16:01:01 -0500

    Set up Home and About page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Home from  './components/pages/Home'
-import About from  './components/pages/About'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 import './App.css';
 
 const App = () => {
@@ -12,10 +12,10 @@ const App = () => {
         <Navbar />
         <div className='container'>
           <Switch>
-            <Route exact path ='/' component={Home} />
-            <Route exact path ='/about' component={About} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
           </Switch>
-          </div>
+        </div>
       </Fragment>
     </Router>
   );
