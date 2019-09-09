be36226810c4f2942ca7ea41877d3b345803f512
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-09-09 16:00:11 -0500

    Set up Home and About page

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 const Navbar = ({ title, icon }) => {
-    return (
-        <div className='navbar bg-primary'>
-            <h1>
-                <i className={icon} /> { title }
-            </h1>
-        </div>
-    )
-}
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </div>
+  );
+};
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string,
-}
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
 
-Navbar.defaultProps= {
- title:'Contact Keeper',
- icon: 'fas fa-id-card-alt'
-}
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt'
+};
 
-export default Navbar
+export default Navbar;
