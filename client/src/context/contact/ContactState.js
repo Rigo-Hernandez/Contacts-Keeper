180a9322e7ec09029345e3e0a8d67c7c49059172
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-09-11 23:18:05 -0500

    Contacts can now be deleted

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -48,7 +48,9 @@ const ContactState = props => {
   };
 
   //Delete Contact
-
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
   //Set current contact
 
   //Clear currnt contact
@@ -63,7 +65,8 @@ const ContactState = props => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        addContact,
+        deleteContact
       }}
     >
       {props.children}
