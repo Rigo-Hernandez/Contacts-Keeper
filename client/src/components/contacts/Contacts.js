34ae2030f6248c88615f0bdd9b3e602c5b33f757
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-09-16 14:41:25 -0500

    Added animation when deleting/adding contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useContext } from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
@@ -15,18 +15,18 @@ const Contacts = () => {
   return (
     <Fragment>
       <TransitionGroup>
-      {filtered != null
-        ? filtered.map(contact => (
-            <CSSTransition key={contact.id} timeout={500} classNames="item">
-            <ContactItem  contact={contact} />
-            </CSSTransition>
-          ))
-        : contacts.map(contact => (
-          <CSSTransition key={contact.id} timeout={500} classNames="item">
-            <ContactItem contact={contact} />
-            </CSSTransition>
-          ))}
-          </TransitionGroup>
+        {filtered != null
+          ? filtered.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
