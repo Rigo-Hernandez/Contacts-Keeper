2972eb4722f6174dcd4570eb6f7e1e2c07323962
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-09-15 20:09:30 -0500

    Adding edit and Update contect action

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,8 +1,10 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
   const contactContext = useContext(ContactContext);
+
+  const {addContact, current} = contactContext
 
   const [contact, setContact] = useState({
     name: '',
@@ -11,11 +13,24 @@ const ContactForm = () => {
     type: 'personal'
   });
 
+  useEffect(() => {
+    if(current !== null){
+      setContact(current)
+    } else {
+      setContact({
+        name: '',
+        email: '',
+        phone: '',
+        type: 'personal'
+      });
+    }
+  },[contactContext, current])
+
   const { name, email, phone, type } = contact;
 
   const onSubmit = e => {
     e.preventDefault();
-    contactContext.addContact(contact);
+    addContact(contact);
     setContact({
       name: '',
       email: '',
@@ -28,7 +43,7 @@ const ContactForm = () => {
     setContact({ ...contact, [e.target.name]: e.target.value });
   return (
     <form onSubmit={onSubmit}>
-      <h2 className='text-primary'>Add Contact</h2>
+      <h2 className='text-primary'>{current ? 'Update Contact': 'Add Contact'}</h2>
       <input
         type='text'
         placeholder='Name'
@@ -70,7 +85,7 @@ const ContactForm = () => {
       <div>
         <input
           type='submit'
-          value='Add contact'
+          value={current ? 'Update Contact': 'Add Contact'}
           className='btn btn-primary btn-block'
         />
       </div>
