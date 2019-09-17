0b3e74beb320367e98d18cc8b909e7938a106be9
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-09-16 20:37:44 -0500

    Connecting authentication context and initial state

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 const Navbar = ({ title, icon }) => {
   return (
     <div className='navbar bg-primary'>
@@ -8,12 +8,18 @@ const Navbar = ({ title, icon }) => {
         <i className={icon} /> {title}
       </h1>
       <ul>
-          <li>
-              <Link to = '/'>Home</Link>
-          </li>
-          <li>
-              <Link to='/about'>About</Link>
-          </li>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+        <li>
+          <Link to='/register'>Register</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
       </ul>
     </div>
   );
