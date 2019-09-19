877c8a3d93e98452c5f824988e6bfd98b1b8ad9e
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-09-19 15:14:54 -0500

    User token tied to localStorage

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,15 +2,15 @@ import React, { useContext, useEffect } from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
 import ContactFilter from '../contacts/ContactFilter';
-import AuthContext from '../../context/auth/authContext'
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
-  const authContext = useContext(AuthContext)
+  const authContext = useContext(AuthContext);
 
-  useEffect(() =>{
+  useEffect(() => {
     authContext.loadUser();
     // eslint-disable-next-line
-  }, [])
+  }, []);
   return (
     <div className='grid-2'>
       <div>
