c14625cdddc88f37bd725f347028aee89abf1a03
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-09-18 23:12:40 -0500

    User registration connected to Backend

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,20 +1,20 @@
 import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
-import AuthContext from '../../context/auth/authContext'
+import AuthContext from '../../context/auth/authContext';
 
 const Register = () => {
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
 
   const { setAlert } = alertContext;
-  const { register, error,clearErrors } = authContext;
+  const { register, error, clearErrors } = authContext;
 
   useEffect(() => {
     if (error === 'User already exists') {
-      setAlert(error, 'danger')
-      clearErrors()
+      setAlert(error, 'danger');
+      clearErrors();
     }
-  }, [error])
+  }, [error]);
 
   const [user, setUser] = useState({
     name: '',
@@ -38,7 +38,7 @@ const Register = () => {
         name,
         email,
         password
-      })
+      });
     }
   };
 
