f7cb59544ba4f73928561bfc831ea4c98b6a4ec1
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-09-19 22:44:28 -0500

    User logout is now functional

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -11,15 +11,15 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
-      case USER_LOADED:
-          return{
-              ...state,
-              isAuthenticated:true,
-              loading: false,
-              user:action.payload
-          }
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload
+      };
     case REGISTER_SUCCESS:
-        case LOGIN_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
         ...state,
@@ -28,9 +28,9 @@ export default (state, action) => {
         loading: false
       };
     case REGISTER_FAIL:
-            case AUTH_ERROR:
-                case LOGIN_FAIL:
-                  case LOGOUT:
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
