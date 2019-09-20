e86849df26a37f4e5c5fc7d37d0ef716d91cc285
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2019-09-19 22:43:58 -0500

    User logout is now functional

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,47 +1,45 @@
 import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import AuthContext from '../../context/auth/authContext'
-
+import AuthContext from '../../context/auth/authContext';
 
 const Navbar = ({ title, icon }) => {
-  const authContext = useContext(AuthContext)
+  const authContext = useContext(AuthContext);
 
-const { isAuthenticated, logout, user } = authContext
+  const { isAuthenticated, logout, user } = authContext;
 
-const onLogout = () => {
-  logout()
-}
+  const onLogout = () => {
+    logout();
+  };
 
-const authLinks = (
-  <Fragment>
-    <li>Hello, { user && user.name }</li>
-    <li>
-      <a onClick={onLogout} href='#!'>
-        <i className='fas fa-sign-out-alt'></i><span className="hide-sm">Logout</span>
-      </a>
-    </li>
-  </Fragment>
-)
-const guestLinks = (
-  <Fragment>
-        <li>
-          <Link to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-  </Fragment>
-)
+  const authLinks = (
+    <Fragment>
+      <li>Hello, {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href='#!'>
+          <i className='fas fa-sign-out-alt'></i>
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </Fragment>
+  );
 
   return (
     <div className='navbar bg-primary'>
       <h1>
         <i className={icon} /> {title}
       </h1>
-      <ul>
-      {isAuthenticated ? authLinks : guestLinks}
-      </ul>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
     </div>
   );
 };
