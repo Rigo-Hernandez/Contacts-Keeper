716dc0844e62813a0a5495f85de45252c3fefbb7
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2019-09-21 15:17:38 -0500

    Privateroute has been added

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,16 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import PrivateRoute from './components/routing/PrivateRoute'
+import PrivateRoute from './components/routing/PrivateRoute';
 import Alerts from './components/layout/Alerts';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
-import setAuthToken from './utils/setAuthToken'
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
-if(localStorage.token){
-  setAuthToken(localStorage.token)
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
 
 const App = () => {
